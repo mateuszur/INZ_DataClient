8c320618de8e5ea0e7e2e18910d40d6c4df1e3fe
Mateusz Urbańczyk <mateurb251@student.polsl.pl>
2023-12-12 21:46:52 +0100

    zatwierdzanie 12.12

--- DataKlient/DataKlient/ViewModels/LoginViewModel.cs
+++ DataKlient/DataKlient/ViewModels/LoginViewModel.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -14,16 +16,28 @@ namespace DataKlient.ViewModels
     public class LoginViewModel : BaseViewModel
     {
        public Command LoginCommand { get; }
+       
 
-     
         private bool isCheckBoxChecked=false;
-        private bool isPasswordEnabled= true;
+        private bool isPasswordEnabled=true;
 
+        private Color serverStatusColor=Color.Red;
+        private readonly Timer _timer;
+
+        /// na potrzeby testów ping-pong
+        
+        
+        private TcpClient _client;
+        private NetworkStream _stream;
+
+        /// na potrzeby testów ping-pong
 
         public LoginViewModel()
         {
            LoginCommand = new Command(OnLoginClicked);
-        
+            
+           _timer= new Timer (CheckServerAvailability, null, 0, 20000 );
+
         }
 
         private  void OnLoginClicked()
@@ -79,10 +93,75 @@ namespace DataKlient.ViewModels
 
         }
 
+        // Metody sprawdzająca dostępność serwera i ustawiająca odpowiedni kolor
 
-         
+        public Color ServerStatusColor
+        {
+            get { return serverStatusColor; }
+            set
+            {
+                if (serverStatusColor != value)
+                {
+                    serverStatusColor = value;
+                    //CheckServerAvailability();
+                    OnPropertyChanged(nameof(ServerStatusColor));
+                }
+            }
+        }
         
+        private async void CheckServerAvailability(object state)
+        {
+            // Logika sprawdzająca dostępność serwera
+            bool isServerAvailable = CheckServer(); // Możesz użyć odpowiedniej logiki do sprawdzenia stanu serwera
+           
+            ServerStatusColor = isServerAvailable ? Color.Green : Color.Red;
+        }
 
 
+        private bool CheckServer()
+        {
+            try
+            {
+                _client = new TcpClient("192.168.1.90", 3333);
+                _stream = _client.GetStream();
+
+                if (SendRequest("PING") == "PONG")
+                {
+                    _client.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }catch (Exception ex) {
+
+               Console.WriteLine(ex.ToString());
+                return false;
+            }
+          
+
+
+        }
+
+
+        public string SendRequest(string request)
+        {
+            byte[] requestData = Encoding.ASCII.GetBytes(request);
+            _stream.Write(requestData, 0, requestData.Length);
+
+            byte[] buffer = new byte[1024];
+            int byteCount = _stream.Read(buffer, 0, buffer.Length);
+
+            string response = Encoding.ASCII.GetString(buffer, 0, byteCount);
+            //Console.WriteLine("Odpowiedź od serwera: " + response);
+            return request;
+        }
+
+
+
+
+
+        // Metody sprawdzająca dostępność serwera i ustawiająca odpowiedni kolor
     }
 }
